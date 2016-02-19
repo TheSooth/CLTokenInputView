42843474eefe0969c082d1eadb9ae7ae5aa57159
TheSooth <thesooth@aol.com>
2016-02-19 15:22:32 +0200

    removed call of textField change in add token method

--- CLTokenInputView/CLTokenInputView/CLTokenInputView.m
+++ CLTokenInputView/CLTokenInputView/CLTokenInputView.m
@@ -128,9 +128,6 @@ static CGFloat const FIELD_MARGIN_X = 4.0; // Note: Same as CLTokenView.PADDING_
         [self.delegate tokenInputView:self didAddToken:token];
     }
 
-    // Clearing text programmatically doesn't call this automatically
-    [self onTextFieldDidChange:self.textField];
-
     [self updatePlaceholderTextVisibility];
     [self repositionViews];
 }
